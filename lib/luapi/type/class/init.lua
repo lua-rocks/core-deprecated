553e840f89d1c21feb0664ed5f48ae9f2994bf1f
luarocks <luarocks@onionmail.org>
2021-08-12 19:52:37 +0300

    ~

--- lib/luapi/type/class/init.lua
+++ lib/luapi/type/class/init.lua
@@ -3,15 +3,6 @@ local Class = Type:extend 'lib.luapi.type.class'
 
 
 --[[
-@ lib.luapi.type.class#typeset (table)
-> name   (string)    First word after tag (Require path format)
-> title  (string) [] Any text at the end
-> parent (string)    Text in parentheses  (Extended from: reqpath or lua type)
-> square (string) [] Text in square brackets
-]]
-
-
---[[
 > parsed_block (lib.luapi.block) []
 ]]
 function Class:init(parsed_block)
