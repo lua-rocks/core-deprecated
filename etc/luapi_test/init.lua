8201db9534f783446b5b5cd8699e460335ac0700
luarocks <luarocks@onionmail.org>
2021-08-15 15:24:14 +0300

    ~

--- etc/luapi_test/init.lua
+++ etc/luapi_test/init.lua
@@ -1,11 +1,10 @@
 -- Temporary stop parsing code, so tag `=` MUST be always used
--- TODO: replace class.path.field with class.path:field in all project
 
 --[[ This file is sandbox for testing luapi
 = etc.luapi_test (table)
 > some_field1 (some_type1) [some_default_value1] Some field title1
 > some_field2 (some_type2) [some_default_value2] Some field title2
-> class2 (etc.luapi_test:type2) TODO: Should be parsed as method
+> class2 (etc.luapi_test:type2) TODO: Should be writed as method
 ]]
 local M = {}
 
